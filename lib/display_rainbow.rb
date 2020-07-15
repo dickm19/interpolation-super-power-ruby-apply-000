8aa75114acdcbd728156e28d3db85cc456dea8f3
Mary Dick <maryclairedick@gmail.com>
2020-07-15 21:51:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors = [])
-  puts colors[0,1,2,3,4,5,6]
+def display_rainbow(colors = ['R','O','Y','G','B','I','V'])
+  puts colors[]
 end
   
